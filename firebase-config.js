f5a025318fc9fcff3387f56957ac5a2ae07d3e1f
njoroge2305-ops <njoroge2305@gmail.com>
2026-06-28 17:15:41 +0300

    commit the file changes

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-
     apiKey: "AIzaSyCRqsGlEQxKEiaEgBuZPFBn_9eHgnqfM5I",
-
     authDomain: "log-in-nexx-41628.firebaseapp.com",
-
     projectId: "log-in-nexx-41628",
     storageBucket: "log-in-nexx-41628.firebasestorage.app",
-
     messagingSenderId: "639480730659",
-
     appId: "1:639480730659:web:8299afe51001557070cd96",
-
 };
 
-export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize and export Auth
+export const auth = getAuth(app);
--- login.js
+++ login.js
@@ -1,37 +1,22 @@
-import { app } from "./firebase-config.js";
+// 1. Import the auth instance you created in your config file
+import { auth } from "./firebase-config.js";
 
-import {
+// 2. Import the login function from the Firebase CDN
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
-    getAuth,
-
-    signInWithEmailAndPassword
-
-} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
-
-const auth = getAuth(app);
-
+// 3. Handle Form Submission
 document.getElementById("loginForm").addEventListener("submit", (e) => {
-
     e.preventDefault();
 
     const email = document.getElementById("email").value;
-
     const password = document.getElementById("password").value;
 
     signInWithEmailAndPassword(auth, email, password)
-
         .then(() => {
-
             alert("Login Successful!");
-
             window.location.href = "main.html";
-
         })
-
         .catch((error) => {
-
             document.getElementById("errorMsg").innerHTML = error.message;
-
         });
-
 });
